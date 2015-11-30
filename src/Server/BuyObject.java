162726327e4268103122c83d00515644fb9a079d
Martin <pownyan@gmail.com>
2015-11-30 16:11:46 +0100

    Added a BuyObject class

---
+++ src/Server/BuyObject.java
@@ -0,0 +1,28 @@
+package Server;
+
+/**
+ * Created by martin on 2015-11-30.
+ */
+public class BuyObject {
+    private String productName;
+    private double price;
+    private String buyer;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getSeller() {
+        return buyer;
+    }
+
+    public BuyObject(String n, Double p, String b){
+        productName = n;
+        price = p;
+        buyer = b;
+    }
+}
