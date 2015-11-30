754fd92fffc2918b605eeb76f2ff1829c6bace16
Rickard Zwahlen <rickardz@kth.se>
2015-11-30 16:43:16 +0100

    Started implementing client

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,10 +1,48 @@
 package Client;
 
-/**
- * Created by martin on 2015-11-26.
- */
+
+import java.util.Scanner;
+
 public class Client {
+    private final String BANKNAME = "Nordea";
+    private Bank.Client bankClient;
+
     public static void main(String [] args){
 
+        System.out.println("Welcome to Skrocket, please login:");
+        Scanner scanner = new Scanner(System.in);
+        String username = scanner.nextLine();
+        System.out.print("Password:");
+        String password = scanner.nextLine();
+        String input = "";
+
+        boolean loginSuccess = true;
+        /* login*/
+        if(loginSuccess)
+        {
+            while(true)
+            {
+                System.out.println("Login successful. available commands are: ");
+                commandList();
+                input = scanner.nextLine();
+                if(input.trim().equals("logout"))
+                    break;
+                    //TODO logout?
+                executeCommand(input);
+
+            }
+
+        }
+
+    }
+
+    private static void executeCommand(String command)
+    {
+
+    }
+
+    private static void commandList()
+    {
+        System.out.println(" sell \n list \n search \n wish \n logout ");
     }
 }
