2ae5f9477133f968afed7c73422536cd4dd45b6f
Martin <pownyan@gmail.com>
2015-11-30 16:00:54 +0100

    Added example files

---
+++ src/Client/Client.java
@@ -0,0 +1,10 @@
+package Client;
+
+/**
+ * Created by martin on 2015-11-26.
+ */
+public class Client {
+    public static void main(String [] args){
+
+    }
+}
---
+++ src/Server/Server.java
@@ -0,0 +1,10 @@
+package Server;
+
+/**
+ * Created by martin on 2015-11-26.
+ */
+public class Server {
+    public static void main(String [] args){
+
+    }
+}
